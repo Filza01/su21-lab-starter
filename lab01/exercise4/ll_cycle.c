8b527890c6ecc956f789459bc0115dab118cf6f4
Filza01 <filza.shahid@10xengineers.ai>
2023-07-25 11:25:34 +0500

    Exercise 4 completed

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,20 @@
+// Copyright [2023] <Filza Shahid>
 #include <stddef.h>
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node *fast_ptr = head;
+    node *slow_ptr = head;
+
+    while (fast_ptr != NULL && fast_ptr->next != NULL
+    && fast_ptr->next->next != NULL) {
+        slow_ptr = slow_ptr->next;
+        fast_ptr = fast_ptr->next->next;
+
+        if (slow_ptr == fast_ptr) {
+            return 1;
+        }
+    }
+    return 0;
 }
