2a52230c4243aabb40cfc914a915e8035a660e94
Filza01 <filza.shahid@10xengineers.ai>
2023-08-02 13:18:59 +0500

    Lab03 RISC-V task completed

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,19 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    # value is in a0 
+    addi t1, x0, 1
+    addi t2, x0, 1
+    addi a0, a0, 1
+    loop:
+        bge t1, a0, done
+        mul t2, t2, t1
+        addi t1, t1, 1
+        j loop
+       
+    done: 
+        add a0, t2, x0
+        jr ra
+    
+    
+
